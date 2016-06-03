94a45b34fbd382da0c1fbf0828538de2d5c3b882
souptikmakarov <souptikb7@gmail.com>
2016-06-03 14:33:59 +0530

    Update urls.py

--- scrape/urls.py
+++ scrape/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+	url(r'^test2/', views.test2, name='test2'),
 	url(r'^test/', views.test, name='test'),
 	url(r'^$',views.index,name = 'index'),
 ]
